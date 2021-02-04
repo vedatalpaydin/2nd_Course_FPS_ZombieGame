48e09ed0cd90d494dea293b000500f3ddae90277
vedatalpaydin <vedatalpaydin7@icloud.com>
2021-02-04 14:10:33 +0300

    19. Orientating And Restricting Movement

--- Assets/FPController.cs
+++ Assets/FPController.cs
@@ -8,11 +8,13 @@ public class FPController : MonoBehaviour
     [SerializeField] private Camera cam;
     
     private float speed = 0.1f;
-    private float xSensitivity = 5f;
-    private float ySensitivity = 5f;
+    private float xSensitivity = 2f;
+    private float ySensitivity = 2f;
+    private float minX = -90f;
+    private float maxX = 90f;
 
     private Quaternion characterRot;
-    private Quaternion camRot;
+    private Quaternion cameraRot;
 
     private Rigidbody rb;
     private CapsuleCollider capsule;
@@ -22,36 +24,52 @@ public class FPController : MonoBehaviour
         rb =GetComponent<Rigidbody>();
         capsule = GetComponent<CapsuleCollider>();
 
-        camRot = cam.transform.localRotation;
+        cameraRot = cam.transform.localRotation;
         characterRot = transform.localRotation;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            rb.AddForce(0,300,0);
+        }
         
     }
 
     private void FixedUpdate()
     {
-        float yRot = Input.GetAxis("Mouse X") * ySensitivity;
-        float xRot = Input.GetAxis("Mouse Y") * xSensitivity;
+        float yRot = Input.GetAxis("Mouse X") * xSensitivity;
+        float xRot = Input.GetAxis("Mouse Y") * ySensitivity;
         
-        camRot *= Quaternion.Euler(-xRot,0,0);
+        cameraRot *= Quaternion.Euler(-xRot,0,0);
         characterRot *= Quaternion.Euler(0,yRot,0);
 
-        transform.localRotation = characterRot;
-        cam.transform.localRotation = camRot;
+        cameraRot = ClampRotationAroundXAxis(cameraRot);
         
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
-        {
-            rb.AddForce(0,300,0);
-        }
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        cam.transform.localRotation = cameraRot;
+        transform.localRotation = characterRot;
 
-        transform.position += new Vector3(x*speed, 0, z*speed);
+        float x = Input.GetAxis("Horizontal")*speed;
+        float z = Input.GetAxis("Vertical")*speed;
+
+        transform.position +=cam.transform.forward * z + cam.transform.right * x;//new Vector3(x, 0, z);
     }
+
+    Quaternion ClampRotationAroundXAxis(Quaternion q)
+    {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+        angleX = Mathf.Clamp(angleX, minX, maxX);
+        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+        return q;
+    }    
 
     bool IsGrounded()
     {
