c460880bd61ea26fdd4a154f448ad67d28cb8f56
vedatalpaydin <vedatalpaydin7@icloud.com>
2021-02-07 00:26:26 +0300

    51. Finite State Machine Part1

--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -5,32 +5,63 @@ using UnityEngine.AI;
 
 public class ZombieController : MonoBehaviour
 {
+    enum STATE
+    {
+        IDLE,
+        WANDER,
+        ATTACK,
+        CHASE,
+        DEAD
+    }
+
+    private STATE state = STATE.IDLE;
+    
     public GameObject target;
     private Animator anim;
     private NavMeshAgent agent;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         anim = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
-        
-        anim.SetBool("IsWalking",true);
+    }
+
+    void TurnOffTrigger()
+    {
+        anim.SetBool("IsWalking", false);
+        anim.SetBool("IsRunning", false);
+        anim.SetBool("IsAttacking", false);
+        anim.SetBool("IsDead", false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(target.transform.position);
-        if (agent.remainingDistance > agent.stoppingDistance)
+        switch (state)
         {
-            anim.SetBool("IsWalking",true);
-            anim.SetBool("IsAttacking",false);
-        }
-        else
-        {
-            anim.SetBool("IsWalking",false);
-            anim.SetBool("IsAttacking",true);
+            case STATE.IDLE:
+                state = STATE.WANDER;
+                break;
+            case STATE.WANDER:
+                if (!agent.hasPath)
+                {
+                    float newX = transform.position.x + Random.Range(-5, 5);
+                    float newZ = transform.position.z + Random.Range(-5, 5);
+                    float newY = Terrain.activeTerrain.SampleHeight(new Vector3(newX, 0, newZ));
+                    Vector3 dest = new Vector3(newX, newY, newZ);
+                    agent.SetDestination(dest);
+                    agent.stoppingDistance = 0;
+                    TurnOffTrigger();
+                    anim.SetBool("IsWalking",true);
+                }
+                break;
+            case STATE.CHASE:
+                break;
+            case STATE.ATTACK:
+                break;
+            case STATE.DEAD:
+                break;
         }
     }
 }
