cea2c86845924465a5db15c4365407e6077db8f5
vedatalpaydin <vedatalpaydin7@icloud.com>
2021-02-06 22:48:45 +0300

    50. Programing Navmesh Agent

---
+++ Assets/Scripts/ZombieController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieController : MonoBehaviour
+{
+    public GameObject target;
+    private Animator anim;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        
+        anim.SetBool("IsWalking",true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.transform.position);
+        if (agent.remainingDistance > agent.stoppingDistance)
+        {
+            anim.SetBool("IsWalking",true);
+            anim.SetBool("IsAttacking",false);
+        }
+        else
+        {
+            anim.SetBool("IsWalking",false);
+            anim.SetBool("IsAttacking",true);
+        }
+    }
+}
