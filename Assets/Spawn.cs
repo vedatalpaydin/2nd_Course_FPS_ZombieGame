4e03ff29f593972a5edad6ce044f77442c4b98d2
vedatalpaydin <vedatalpaydin7@icloud.com>
2021-02-07 13:59:42 +0300

    55. Spawnin Part 2

--- Assets/Spawn.cs
+++ Assets/Spawn.cs
@@ -1,14 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class Spawn : MonoBehaviour
 {
     public GameObject zombiePrefab;
     public int number;
     public float spawnRadius;
+    public bool spawnOnStart = true;
+
     void Start()
+    {
+        if(spawnOnStart)
+            SpawnAll();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!spawnOnStart && other.tag=="Player")
+            SpawnAll();
+    }
+
+    void SpawnAll()
     {
         for (int i = 0; i < number; i++)
         {
