5e4e6af9620ab5ab60598a00ecabcf0fc3a03723
vedatalpaydin <vedatalpaydin7@icloud.com>
2021-02-08 12:35:21 +0300

    61. Burying the Undead

---
+++ Assets/Scripts/Sink.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sink : MonoBehaviour
+{
+    private float destroyHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartSink()
+    {
+        destroyHeight = Terrain.activeTerrain.SampleHeight(transform.position )-5;
+        Collider[] colList = transform.GetComponentsInChildren<Collider>();
+        foreach (Collider c in colList)
+        {
+            Destroy(c);
+        }
+        InvokeRepeating(nameof(SinkIntoGround),5,0.5f);
+    }
+
+    void SinkIntoGround()
+    {
+        transform.Translate(0,-0.0001f,0);
+        if (transform.position.y < destroyHeight)
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -136,6 +136,8 @@ public class ZombieController : MonoBehaviour
                     state = STATE.CHASE;
                 break;
             case STATE.DEAD:
+                Destroy(agent);
+                GetComponent<Sink>().StartSink();
                 break;
         }
     }
