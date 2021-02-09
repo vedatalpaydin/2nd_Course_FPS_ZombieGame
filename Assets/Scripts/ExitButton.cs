d90ce479d2d3d1a5c45b9b54a622c53bc9580af8
vedatalpaydin <vedatalpaydin7@icloud.com>
2021-02-09 18:29:37 +0300

    90. Volume Controls

--- Assets/Scripts/ExitButton.cs
+++ Assets/Scripts/ExitButton.cs
@@ -1,10 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ExitButton : MonoBehaviour
 {
+
+    public Slider musicSlider;
+    private GameObject gController;
+    private void Awake()
+    {
+        gController = GameObject.Find("GameController");
+        if(gController == null) return;
+        AudioSource music = gController.GetComponent<AudioSource>();
+        musicSlider.value = music.volume;
+    }
+
     public void ExitScene()
     {
         SceneManager.LoadScene("MainMenu");
@@ -12,5 +25,12 @@ public class ExitButton : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene("GameLevel");
+    }
+
+    public void ChangeVolume(float volume)
+    {
+        if(gController == null) return;
+        AudioSource music = gController.GetComponent<AudioSource>();
+        music.volume = volume;
     }
 }
